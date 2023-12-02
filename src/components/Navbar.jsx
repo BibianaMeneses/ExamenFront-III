fe5f3ff53ee4e0c08aa541774823f7c92dd2ff5a
Olguita1981 <olgagil1981@gmail.com>
2023-12-02 02:45:16 -0500

    Agregado cambio de tema

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useDarkMode from "./utils/useDarkMode";
 
 const Navbar = () => {
+  const [darkMode, toggleDarkMode] = useDarkMode();
   return (
-    <div className="bg-gray-100 flex">
+    <div className="bg-gray-400 flex">
       <div className="block">
         <h3 className="text-2xl font-semibold">
           <span className="text-red-600 font-bold">DH</span> Odontología Olga&Bibiana
@@ -20,8 +22,8 @@ const Navbar = () => {
           <h4 className="font-semibold">Favoritos</h4>
         </Link>
         <div className="pt-1">
-        <button className="font-semibold" id="cambiarTema">Tema</button>
-      </div>
+          <button className="toggleButton" onClick={() => toggleDarkMode()}>{darkMode ? 'Light' : 'Dark'}</button>
+        </div>
       </nav>
     </div>
   );
